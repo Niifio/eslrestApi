657efefc6a353379afcae96547ccddd721eebf74
niifio <josephniifio@yahoo.com>
2023-02-05 02:18:28 +0400

    my first commit

---
+++ api.js
@@ -0,0 +1,6 @@
+const Data = [
+  { question: "Backyard", answer: "1" },
+  { question: "Jewelry", answer: "2" },
+  { question: "Approach", answer: "3" },
+  { question: "Headhunter", answer: "4" },
+];
