f2e417b8e1649d848a1e292b35a1a7804a3df61d
niifio <josephniifio@yahoo.com>
2023-02-05 02:29:14 +0400

    made changes

--- api.js
+++
@@ -1,6 +0,0 @@
-const Data = [
-  { question: "Backyard", answer: "1" },
-  { question: "Jewelry", answer: "2" },
-  { question: "Approach", answer: "3" },
-  { question: "Headhunter", answer: "4" },
-];
